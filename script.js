4417e40b59bf5ada1a6293fa3f983bf0a2e0037c
Molid <delmolid@hotmail.fr>
2024-12-27 15:13:59 +0100

    creation saut de section ou de page

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-index.html
-script.js
+function handleSectionScrollViaArrowIcons() {
+    document.querySelectorAll(".arrow-down").forEach(e => {
+        e.addEventListener("click", function() {
+            var t = this.closest("section");
+            if (t) {
+                let e = t.nextElementSibling;
+                if (window.innerWidth < 576 || window.innerHeight < 600) {
+                    for (; e && 0 === e.offsetHeight;)
+                        e = e.nextElementSibling;
+                    if (!e)
+                        return
+                }
+                e.scrollIntoView({
+                    behavior: "smooth"
+                })
+            }
+        })
+    })
+}
